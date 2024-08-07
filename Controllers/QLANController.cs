b8c4c2bf889dc7bdf5c5a99ec1d070f51b03a423
JohnWickCodeTesster <quan.lm9037@sinhvien.hoasen.edu.vn>
2024-08-07 23:46:13 +0700

    frontend

---
+++ Controllers/QLANController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DoAnChuyenNganh.Controllers
+{
+    public class QLANController : Controller
+    {
+        public IActionResult DiemDanh()
+        {
+            return View();
+        }
+
+        public IActionResult TrangChu()
+        {
+            return View();
+        }
+        public IActionResult DangKy()
+        {
+            return View();
+        }
+        public IActionResult Diem()
+        {
+            return View();
+        }
+    }
+}
